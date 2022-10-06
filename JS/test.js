d76ff659392ae96a2090b786ca365f8e8ad454e3
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2022-10-06 16:24:29 +0300

    self_test_2_11

---
+++ JS/test.js
@@ -0,0 +1,13 @@
+// self_check_2_11
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//   const words = message.split(" ");
+//   console.log(words);
+
+//   const total = words.length;
+//   console.log(total);
+
+//   console.log(total * pricePerWord);
+// }
+
+// calculateEngravingPrice("JavaScript is in my blood", 10);
