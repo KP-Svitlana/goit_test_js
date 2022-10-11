7ac61cf62d24330bc8d8abaa552e55222ebf1c40
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2022-10-11 18:59:14 +0300

    added lection_repeta_3_1

---
+++ JS/lection_repeta_3.js
@@ -0,0 +1,45 @@
+// const playlist = {
+//   name: "Romance music",
+//   style: "Romance",
+//   rating: 5.0,
+//   tracks: ["track-1", "track-2", "track-3"],
+//   trackCount: 3,
+//   getNate: function () {
+//     console.log("Yey! It is a function");
+//   },
+//   getName() {
+//     console.log("Yey! It is a function");
+//   },
+// };
+
+// playlist.qwe = 5;
+// playlist.rating = 10;
+
+// console.log(playlist);
+
+// const propertyName = "tracks";
+
+// console.log(playlist["rating"]);
+// console.log(playlist[propertyName]);
+
+// Короткий запис властивостей
+// const username = "Mango";
+// const email = "mango@mail.com";
+
+// const singupData = {
+//   username,
+//   email,
+// };
+
+// console.log(singupData);
+
+// Вираховуємі властивості
+
+// const inputName = "color";
+// const inputValue = "tomato";
+
+// const colorPickerDate = {
+//   [inputName]: inputValue,
+// };
+
+// console.log(colorPickerDate);
