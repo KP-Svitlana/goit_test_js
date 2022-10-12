0e503eaba9f988bf65ea79ab1ffdf2241d9e170d
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2022-10-12 19:21:37 +0300

    add half of repeta lection_3_1

---
+++ JS/lection_repeta_3_1.js
@@ -0,0 +1,23 @@
+// function myltiply(...args) {
+//   console.log(args);
+// }
+
+// function myltiply(firstNumber, secondNumber, ...otherArgs) {
+//   console.log(firstNumber);
+//   console.log(secondNumber);
+//   console.log(otherArgs);
+// }
+
+// myltiply(1, 2, 3);
+// myltiply(1, 2, 3, 4);
+// myltiply(1, 2, 3, 4, 5);
+
+// const numbers = [1, 2, 3].concat([4, 5, 6], [7, 8, 9]);
+// const numbers = [...[1, 2, 3], 4, 5];
+
+// console.log(numbers);
+
+// const temps = [18, 19, 21, 45, 9, 17];
+
+// console.log(Math.max(1, 2, 5, 9, 17));
+// console.log(Math.max(...temps));
