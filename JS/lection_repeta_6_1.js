c1cffa8882fe7f7697965badf5d02f91d0fea344
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2022-11-08 00:10:07 +0200

    lection 6_1 Repeta

--- JS/lection_repeta_6_1.js
+++ JS/lection_repeta_6_1.js
@@ -1,1 +1,25 @@
-const buttons = document.querySelector(".item-btn");
+// const buttonEl = document.querySelector(".list-of-buttons__btn");
+// console.log(buttonEl);
+
+// const buttonsEl = document.querySelectorAll(".list-of-buttons__btn");
+// console.log(buttonsEl);
+
+// const listEl = document.querySelector("ul");
+// console.log(listEl);
+
+// const itemsEl = document.querySelectorAll("li");
+// console.log(itemsEl);
+
+// const buttonEl = listEl.querySelector("li");
+// console.log(buttonEl);
+
+// const imageEl = document.querySelector(".image");
+// console.log("imageEl", imageEl);
+// console.log(imageEl.src);
+
+// imageEl.src = "http://127.0.0.1:5500/img/2.jpg";
+// imageEl.alt = "sanflower";
+
+// const heroTitleEL = document.querySelector(".hero__title");
+// console.log(heroTitleEL);
+// console.log(heroTitleEL.textContent);
