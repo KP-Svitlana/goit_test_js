e2850ea3afdbf2c1a45b77f2a28a6f4596e2f679
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2022-11-13 09:21:41 +0200

    bank account task first try

---
+++ JS/bank_account_task.js
@@ -0,0 +1,26 @@
+const Transaction = {
+  DEPOSIT: "deposit",
+  WITHDRAW: "withdraw",
+};
+
+const account = {
+  balance: 0,
+  transactions: [],
+
+  createTransaction(amount, type) {
+    const newTransaction = {
+      id: `${this.transactions.length + 1}__${type}`,
+      type,
+      amount,
+    };
+    return newTransaction;
+  },
+
+  deposit(amount) {
+    this.createTransaction(amount, Transaction.DEPOSIT);
+    this.balance += amount;
+    // this.transactions.push(this.createTransaction);
+  },
+};
+
+console.log(account.deposit(1000));
