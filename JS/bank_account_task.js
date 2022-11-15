8584467de7443934868bf1035b53914d195fa919
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2022-11-15 15:46:00 +0200

    added 4 tasks

--- JS/bank_account_task.js
+++ JS/bank_account_task.js
@@ -1,26 +1,26 @@
-const Transaction = {
-  DEPOSIT: "deposit",
-  WITHDRAW: "withdraw",
-};
+// const Transaction = {
+//   DEPOSIT: "deposit",
+//   WITHDRAW: "withdraw",
+// };
 
-const account = {
-  balance: 0,
-  transactions: [],
+// const account = {
+//   balance: 0,
+//   transactions: [],
 
-  createTransaction(amount, type) {
-    const newTransaction = {
-      id: `${this.transactions.length + 1}__${type}`,
-      type,
-      amount,
-    };
-    return newTransaction;
-  },
+//   createTransaction(amount, type) {
+//     const newTransaction = {
+//       id: `${this.transactions.length + 1}__${type}`,
+//       type,
+//       amount,
+//     };
+//     return newTransaction;
+//   },
 
-  deposit(amount) {
-    this.createTransaction(amount, Transaction.DEPOSIT);
-    this.balance += amount;
-    // this.transactions.push(this.createTransaction);
-  },
-};
+//   deposit(amount) {
+//     this.createTransaction(amount, Transaction.DEPOSIT);
+//     this.balance += amount;
+//     // this.transactions.push(this.createTransaction);
+//   },
+// };
 
-console.log(account.deposit(1000));
+// console.log(account.deposit(1000));
--- JS/codewars.js
+++ JS/codewars.js
@@ -6,44 +6,67 @@
 // "Same" means, here, that the elements in b are the elements in a squared,
 //  regardless of the order.
 
-a = [121, 144, 19, 161, 19, 144, 19, 11];
-b = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
+// a = [121, 144, 19, 161, 19, 144, 19, 11];
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
 
-a2 = [121, 144, 19, 161, 19, 144, 19, 11];
-b2 = [132, 14641, 20736, 361, 25921, 361, 20736, 361];
-
-// a.forEach((element) => {
-//   let el = Math.pow(element, 2);
-//   if (b.includes(el)) {
-//     console.log("true");
-//   } else {
-//     console.log("false");
-//   }
-// });
+// a2 = [121, 144, 19, 161, 19, 144, 19, 11];
+// b2 = [132, 14641, 20736, 361, 25921, 361, 20736, 361];
 
 // function comp(array1, array2) {
-//   let result = false;
-//   array1.forEach((element) => {
-//     let el = Math.pow(element, 2);
-//     if (array2.includes(el)) {
-//       console.log((result = true));
-//     } else {
-//       console.log(result);
-//     }
-//   });
+//   const result = array1
+//     .map((element) => Math.pow(element, 2))
+//     .every((el) => array2.includes(el));
+//   return result;
 // }
 
-// function comp(array1, array2) {
-//   let result = false;
-//   array1.forEach((element) => {
-//     let el = Math.pow(element, 2);
-//     if (array2.includes(el)) {
-//       console.log((result = true));
-//     } else {
-//       console.log((result = false));
-//     }
-//   });
+// comp(a, b);
+
+// task_2
+// Convert number to reversed array of digits
+// Given a random non-negative number,
+// you have to return the digits of this number within
+// an array in reverse order.
+
+// EX: 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+// function digitize(n) {
+//   const string = `${n}`;
+
+//   const result = string
+//     .split("")
+//     .reverse()
+//     .map((el) => Number(el));
+//   return result;
 // }
 
-// // comp(a, b);
-// console.log(comp(a2, b2));
+// console.log(digitize(12345));
+
+// task_3
+// function greet(name) {
+//   return console.log(`"Hello, ${name} how are you doing today?"`);
+// }
+
+// function greet(name) {
+//   const string = `'Hello, ${name} how are you doing today?'`;
+//   console.log(string);
+//   return string;
+// }
+
+// greet("Rayn");
+
+// task_4
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns
+// the time since midnight in milliseconds.
+
+function getFoundMillisec(h, m, s) {
+  const minutes = h * 60;
+  const seconds = (minutes + m) * 60;
+  const millisec = (seconds + s) * 1000;
+
+  return millisec;
+}
+
+getFoundMillisec(0, 1, 1);
