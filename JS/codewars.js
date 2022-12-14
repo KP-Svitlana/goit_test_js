853b8183da3f9ad31ef1ec291e0a8f5837d25d5e
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2022-12-14 20:33:04 +0200

    Update codewars.js

--- JS/codewars.js
+++ JS/codewars.js
@@ -857,3 +857,31 @@
 // }
 
 // fakeBin("45385593107843568");
+
+// Task_49
+
+// var summation = function (num) {
+//   const array = [];
+//   for (let i = 1; i <= num; i += 1) {
+//     array.push(i);
+//   }
+
+//   return array.reduce((t, el) => (t += el), 0);
+// };
+
+// console.log(summation(8));
+
+// Task_50
+
+// function getMiddle(s) {
+//   const length = s.length;
+//   if (length % 2 === 0) {
+//     return `${s[length / 2 - 1]}${s[length / 2]}`;
+//   } else {
+//     return `${s[Math.round(length / 2 - 1)]}`;
+//   }
+// }
+
+// console.log(getMiddle("test"));
+// console.log(getMiddle("testing"));
+// console.log(getMiddle("middle"));
